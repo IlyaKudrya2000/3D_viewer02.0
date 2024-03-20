c69a9902299a52738ec766476249eb1e3e8443c3
IlyaKudrya2000 <138864366+IlyaKudrya2000@users.noreply.github.com>
2024-03-20 04:24:01 -0700

    Update VAO.cs

--- Models/VAO.cs
+++ Models/VAO.cs
@@ -1,87 +1,253 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
+
+
 using OpenTK.Graphics.OpenGL;
-using _3D_viewer.Models;
-using System.Timers;
-namespace _3D_viewer.ViewModels
+using OpenTK.Mathematics;
+
+namespace _3D_viewer.Models
 {
-    internal class VaoManager
+    internal class VAO
     {
-        public uint[] vboArrays;
-        public uint[] VAOIds;
-        public int vboArrayCount = 0;
-        public int VAOCount = 0;
-        public ShaderProgram shaderProgram;
-        public List<VAO> VAOs;
-        public List<int> currentVAO;
-        /// <summary>Вызвать при загрузке OpenGL</summary>
-        public VaoManager(string vertexfile, string fragmentfile)
+        ///////// VBO
+        private uint QuadsVboNormal;
+        private uint QuadsVboVertex;
+        private int QuadsCount;
+        private uint TrianglesVboNormal;
+        private uint TrianglesVboVertex;
+        private int TrianglesCount;
+        private uint IDTriangles;
+        private uint IDQuads;
+        Matrix4 modelMatrix;
+        Matrix4 localMatrix;
+        Matrix4 viewMatrix;
+        Matrix4 projectionMatrix;
+        private float[] AngleLocal;
+        private float[] AngleModel;
+        private float[] AngleView;
+        private float[] PositionLocal;
+        private float[] PositionModel;
+        private float[] PositionView;
+        private const int localModFlag = 0;
+        private const int ModelModFlag = 1;
+        #region Получить Данные о положении модели
+        /// <summary>0 - Local matrix , 1 - model matrix</summary>
+        public float[] GetAngle(int modFlag = ModelModFlag) => modFlag == localModFlag ? AngleLocal : AngleModel;
+
+
+        /// <summary>0 - Local matrix , 1 - model matrix</summary>
+        public float[] GetPosition(int modFlag = ModelModFlag) => modFlag == localModFlag ? PositionLocal : PositionModel;
+
+
+        #endregion
+        #region Установщики положения в пространстве 
+        
+        #region Матрица проекции
+        public void SetProjectionMatrix(float fieldOfView, float aspectRatio, float nearPlaneDistance, float farPlaneDistance)
         {
-            
-            VAOs = new List<VAO>();
-            currentVAO = new List<int>();
-            shaderProgram = new ShaderProgram(vertexfile, fragmentfile);
-            InitiVboIndex(0);
-            InitiVAOId(0);
-            
+            Matrix4.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearPlaneDistance, (float)Math.Pow(10, 35), out projectionMatrix);
         }
-        public void DeleteVAO(int ID)
+        #endregion
+        public void SetModelMatrix(float angle, float axisX, float axisY, float axisZ)
         {
-            GL.DeleteVertexArray(VAOIds[ID]);
-           // VAOs[ID].Delete((uint)ID);
-            VAOs.RemoveAt(ID);
-            VAOCount--;
+            modelMatrix = Matrix4.CreateFromAxisAngle(new Vector3(axisX, axisY, axisZ), 0.0175f * angle);
         }
-        private void InitiVboIndex(int count)
+        #region Вращение
+        public void SetLocalRotation(float angle, float axisX, float axisY, float axisZ)
         {
-            count = 4 * 1000; // 4 - количество индексов для одной модели, 20 - количество моделей 
-
-            vboArrays = new uint[count];
-            GL.GenBuffers(count, vboArrays);
-
-
+            AngleLocal[0] = axisX == 0 ? AngleLocal[0] : angle * axisX;
+            AngleLocal[1] = axisY == 0 ? AngleLocal[1] : angle * axisY;
+            AngleLocal[2] = axisZ == 0 ? AngleLocal[2] : angle * axisZ;
         }
-        private void InitiVAOId(int count)
+        public void SetLocalPosition(float axisX, float axisY, float axisZ)
         {
-            count = 1000; // 20 - количество моделей 
-
-            VAOIds = new uint[count];
-            GL.GenVertexArrays(count, VAOIds);
-
-
+            PositionLocal[0] = axisX == 0 ? PositionLocal[0] : axisX;
+            PositionLocal[1] = axisY == 0 ? PositionLocal[1] : axisY;
+            PositionLocal[2] = axisZ == 0 ? PositionLocal[2] : axisZ;
         }
-        public VAO AddGeometricModel(string fileName)
+        public void SetModelRotation(float angle, float axisX, float axisY, float axisZ)
         {
-
-            GeometricModel model = new GeometricModel(fileName);
-            VAOs.Add(new VAO(model, vboArrays, vboArrayCount, VAOIds, VAOCount));
-
-
-            VAOCount++;
-
-
-            return VAOs[VAOCount - 1];
+            AngleModel[0] = axisX * angle;
+            AngleModel[1] = axisY * angle;
+            AngleModel[2] = axisZ * angle;
         }
-        public void Draw()
+        #endregion
+        #region Положение
+        public void SetModelPosition(float axisXPos, float axisYPos, float axisZPos)
         {
-              for (int i = 0; i < VAOCount; i++)
-              {
-                  VAOs[i].Draw(shaderProgram);
-              }   
-            
-        }
-        public void SetCurrentModel(List<int> Models)
-        {
-            currentVAO.Clear();
-            for (int i = 0; i < Models.Count; i++)
-            {
-                currentVAO.Add(Models[i]);
-            }
+            AngleModel[0] = axisXPos;
+            AngleModel[1] = axisYPos;
+            AngleModel[2] = axisZPos;
         }
         
+        #endregion
+        #endregion
+        public void RotateMatrix(float angle, float axisX, float axisY, float axisZ, string matrixName)
+        {
+            Vector3 rotate = new Vector3(axisX, axisY, axisZ);
+            
+            switch (matrixName)
+            {
+                case "modelMatrix":
+                    AngleModel[0] += axisX * angle;
+                    AngleModel[1] += axisY * angle;
+                    AngleModel[2] += axisZ * angle;
+                   // modelMatrix = modelMatrix * Matrix4.CreateFromAxisAngle(rotate, angle);
+                    break;
+                case "localMatrix":
+                    AngleLocal[0] += axisX * angle;
+                    AngleLocal[1] += axisY * angle;
+                    AngleLocal[2] += axisZ * angle;
+                   // localMatrix = localMatrix * Matrix4.CreateFromAxisAngle(rotate, angle);
+                    break;
+                case "viewMatrix":
+                    //viewMatrix = viewMatrix * Matrix4.CreateFromAxisAngle(rotate, angle);
+                    break;
+                    
+            }
+        }
+        public void ShiftMatrix(float x, float y, float z, string matrixName)
+        {
+            switch (matrixName)
+            {
+                case "modelMatrix":
+                    PositionModel[0] += x;
+                    PositionModel[1] += y;
+                    PositionModel[2] += z;
+                   // modelMatrix = modelMatrix * Matrix4.CreateTranslation(x, y, z);
+                    break;
+                case "localMatrix":
+                    PositionLocal[0] += x;
+                    PositionLocal[1] += y;
+                    PositionLocal[2] += z;
+                  //  localMatrix = localMatrix * Matrix4.CreateTranslation(x, y, z) ;
+                    break;
+                case "viewMatrix":
+                    PositionView[0] += x;
+                    PositionView[1] += y;
+                    PositionView[2] += z;
+                    
+                    break;
+            }
+        }
+
+        public void ChangeViewMatrix(float x, float y, float z)
+        {
+            viewMatrix = viewMatrix * Matrix4.CreateTranslation(x, y, z);
+        }
+
+        public VAO(GeometricModel model, uint[] VBOs, int vboArrayCount, uint[] VAOId, int VAOCount)
+        {
+
+
+            TrianglesVboNormal = VBOs[0 + vboArrayCount * 4];
+            TrianglesVboVertex = VBOs[1 + vboArrayCount * 4];
+            QuadsVboNormal = VBOs[2 + vboArrayCount * 4];
+            QuadsVboVertex = VBOs[3 + vboArrayCount * 4];
+            IDQuads = VAOId[0 + VAOCount * 2];
+            IDTriangles = VAOId[1 + VAOCount * 2];
+            CreateVBO(model.GetTriangNormalsArrayForVbo(), VBOs[0 + vboArrayCount * 4]);
+            CreateVBO(model.GetTriangsArrayForVbo(), VBOs[1 + vboArrayCount * 4]);
+            CreateVBO(model.GetQuadNormalsArrayForVbo(), VBOs[2 + vboArrayCount * 4]);
+            CreateVBO(model.GetQuadsArrayForVbo(), VBOs[3 + vboArrayCount * 4]);
+            QuadsCount = model.GetQuadsCount();
+            TrianglesCount = model.GetTrianglesCount();
+
+            projectionMatrix = Matrix4.CreatePerspectiveFieldOfView(0.785398f, 2f, 0.1f, (float)Math.Pow(10, 35));
+            viewMatrix = Matrix4.CreateTranslation(0, 0, -10);
+            //localMatrix = Matrix4.CreateTranslation(0, 0, 0);
+            //modelMatrix = Matrix4.CreateTranslation(0, 0, 0);
+            // modelMatrix = Matrix4.CreateFromAxisAngle(new Vector3(0.0f, 1.0f, 0.0f), -0.785398f);
+            AngleLocal = new float[3];
+            AngleModel = new float[3];
+            AngleView = new float[3];
+            PositionLocal = new float[3];
+            PositionModel = new float[3];
+            PositionView = new float[3];
+            PositionView[2] = -10;
+            CreateVAO(TrianglesVboVertex, TrianglesVboNormal, IDTriangles);
+            CreateVAO(QuadsVboVertex, QuadsVboNormal, IDQuads);
+
+        }
+
+        private void CreateVBO(float[] data, uint vbo)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, data.Length * sizeof(float), data, BufferUsageHint.StaticDraw); // data - vertices
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+        public void Draw(ShaderProgram shaderProgram)
+        {
+            DrawVAO(TrianglesCount, 3, PrimitiveType.Triangles, IDTriangles, shaderProgram);
+            DrawVAO(QuadsCount, 4, PrimitiveType.Quads, IDQuads, shaderProgram);
+        }
+        private void DrawVAO(int Count, int angles, PrimitiveType shape, uint ID, ShaderProgram shaderProgram)
+        {
+            shaderProgram.ActiveProgram();
+
+            modelMatrix =
+                  Matrix4.CreateFromAxisAngle(new Vector3(0, 0, 1), AngleModel[2] * (float)Math.PI / 180)
+                * Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), AngleModel[1] * (float)Math.PI / 180)
+                * Matrix4.CreateFromAxisAngle(new Vector3(1, 0, 0), AngleModel[0] * (float)Math.PI / 180)
+                * Matrix4.CreateTranslation(PositionModel[0], PositionModel[1], PositionModel[2]);
+
+            localMatrix =
+                  Matrix4.CreateFromAxisAngle(new Vector3(0, 0, 1), AngleLocal[2] * (float)Math.PI / 180)
+                * Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), AngleLocal[1] * (float)Math.PI / 180)
+                * Matrix4.CreateFromAxisAngle(new Vector3(1, 0, 0), AngleLocal[0] * (float)Math.PI / 180)
+                * Matrix4.CreateTranslation(PositionLocal[0], PositionLocal[1], PositionLocal[2]);
+
+            viewMatrix = Matrix4.CreateTranslation(PositionView[0], PositionView[1], PositionView[2]);
+            shaderProgram.SetUniform4("projection", projectionMatrix);
+            shaderProgram.SetUniform4("model", modelMatrix);
+            shaderProgram.SetUniform4("view", viewMatrix);
+            shaderProgram.SetUniform4("local", localMatrix);
+            GL.BindVertexArray(ID);
+            GL.DrawArrays(shape, 0, Count * angles);
+            shaderProgram.DeactiveProgram();
+
+        }
+        private void CreateVAO(uint VboVertex, uint VboNormal, uint ID)
+        {
+
+
+            GL.BindVertexArray(ID);
+
+            GL.EnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(1);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VboVertex);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VboNormal);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            GL.BindVertexArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DisableVertexAttribArray(1);
+            GL.DisableVertexAttribArray(0);
+
+            // Удаляем VBO
+            DeleteVBO(VboVertex, VboNormal);
+        }
+        private void DeleteVBO(uint vboVertex, uint vboNormal)
+        {
+            GL.DeleteBuffer(vboVertex);
+            GL.DeleteBuffer(vboNormal);
+            //GL.DeleteBuffer(vboColor);
+        }
+        public void Delete(uint ID)
+        {
+          //  GL.BindVertexArray(0);
+            GL.DeleteVertexArray(ID);
+        }
+        
+
+
     }
 }
